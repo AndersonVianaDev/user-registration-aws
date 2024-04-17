326c2c3972db5c7401ef2cdae3aca8780ace2caf
Anderson Viana <andersonp.viana.contato@gmail.com>
2024-04-17 14:27:24 -0300

    feat: changing sns to sqs and adding queue url

--- ms-user/src/main/java/com/anderson/msuser/producer/Producer.java
+++ ms-user/src/main/java/com/anderson/msuser/producer/Producer.java
@@ -2,25 +2,28 @@ package com.anderson.msuser.producer;
 
 import com.anderson.msuser.core.user.dtos.EmailDTO;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.awspring.cloud.sns.core.SnsTemplate;
+import io.awspring.cloud.sqs.operations.SqsTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Producer {
 
-    private final SnsTemplate snsTemplate;
+    private final SqsTemplate sqsTemplate;
+    private final ObjectMapper mapper;
 
     @Value("url-queue")
     private String queue;
 
-    public Producer(SnsTemplate snsTemplate, ObjectMapper mapper) {
-        this.snsTemplate = snsTemplate;
+    public Producer(SqsTemplate sqsTemplate, ObjectMapper mapper) {
+        this.sqsTemplate = sqsTemplate;
+        this.mapper = mapper;
     }
 
     public void publishMessageEmail(EmailDTO dto) {
         try {
-            snsTemplate.convertAndSend(queue, dto);
+            String dtoString = mapper.writeValueAsString(dto);
+            sqsTemplate.send(queue, dtoString);
         } catch(Exception e) {
             throw new RuntimeException("");
         }
