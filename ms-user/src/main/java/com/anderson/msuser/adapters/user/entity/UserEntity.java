8f0f1cfc4676e463e0858a86f5b15739bd1efa54
Anderson Viana <andersonp.viana.contato@gmail.com>
2024-04-18 10:08:11 -0300

    feat: implementing user details

--- ms-user/src/main/java/com/anderson/msuser/adapters/user/entity/UserEntity.java
+++ ms-user/src/main/java/com/anderson/msuser/adapters/user/entity/UserEntity.java
@@ -2,12 +2,18 @@ package com.anderson.msuser.adapters.user.entity;
 
 import com.anderson.msuser.core.user.enums.UserType;
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
 @Table(name = "tb_users")
-public class UserEntity {
+public class UserEntity implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -40,8 +46,43 @@ public class UserEntity {
         return email;
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+
+        if(this.userType == UserType.ADMIN) authorities.addAll(List.of(new SimpleGrantedAuthority("ROLE_ADMIN")));
+        else authorities.addAll(List.of(new SimpleGrantedAuthority("ROLE_COMMON")));
+
+        return authorities;
+    }
+
     public String getPassword() {
         return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
     public UserType getUserType() {
