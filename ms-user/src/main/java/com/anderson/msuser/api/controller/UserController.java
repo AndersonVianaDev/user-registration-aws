e6256d490359dd559f6368fdd3bae1ae4be1f244
Anderson Viana <andersonp.viana.contato@gmail.com>
2024-04-18 10:06:42 -0300

    feat: adding method find user by email

--- ms-user/src/main/java/com/anderson/msuser/api/controller/UserController.java
+++ ms-user/src/main/java/com/anderson/msuser/api/controller/UserController.java
@@ -3,7 +3,6 @@ package com.anderson.msuser.api.controller;
 import com.anderson.msuser.core.user.dtos.UserDTO;
 import com.anderson.msuser.core.user.dtos.UserResponseDTO;
 import com.anderson.msuser.core.user.services.UserService;
-import com.anderson.msuser.mapper.UserMapper;
 import com.anderson.msuser.api.validation.UserRequestDTO;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
@@ -13,21 +12,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import static com.anderson.msuser.mapper.UserMapper.toUserDTO;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
     private final UserService service;
-    private final UserMapper userMapper;
 
-    public UserController(UserService service, UserMapper userMapper) {
+    public UserController(UserService service) {
         this.service = service;
-        this.userMapper = userMapper;
     }
 
     @PostMapping("/register")
-    public ResponseEntity<UserResponseDTO> register(@RequestBody @Valid UserRequestDTO userRequestDTO) {
-        UserDTO dto = userMapper.toUserDTO(userRequestDTO);
+    public ResponseEntity<UserResponseDTO> register(@RequestBody @Valid UserRequestDTO data) {
+        UserDTO dto = toUserDTO(data);
         UserResponseDTO response = service.insert(dto);
 
         return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequest()
--- ms-user/src/main/java/com/anderson/msuser/core/user/repository/UserRepository.java
+++ ms-user/src/main/java/com/anderson/msuser/core/user/repository/UserRepository.java
@@ -2,7 +2,10 @@ package com.anderson.msuser.core.user.repository;
 
 import com.anderson.msuser.core.user.model.User;
 
+import java.util.Optional;
+
 public interface UserRepository {
 
     User save(User user);
+    Optional<User> findByEmail(String email);
 }
