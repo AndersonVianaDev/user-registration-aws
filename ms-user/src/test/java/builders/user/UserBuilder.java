4ba8313d93437aa82c0711b8a5e125e884fc82d9
Anderson Viana <andersonp.viana.contato@gmail.com>
2024-04-18 15:37:23 -0300

    test: improving the parameters

--- ms-user/src/test/java/builders/user/UserBuilder.java
+++ ms-user/src/test/java/builders/user/UserBuilder.java
@@ -1,5 +1,6 @@
 package builders.user;
 
+import com.anderson.msuser.api.validation.UserRequestDTO;
 import com.anderson.msuser.core.user.dtos.UserDTO;
 import com.anderson.msuser.core.user.enums.UserType;
 import com.anderson.msuser.core.user.model.User;
@@ -8,10 +9,13 @@ import java.util.UUID;
 
 public class UserBuilder {
 
+    public static UserRequestDTO toUserRequestDTO() {
+        return new UserRequestDTO("Anderson", "anderson@gmail.com", "Anderson");
+    }
     public static UserDTO toUserDTO() {
-        return new UserDTO("name", "email", "password");
+        return new UserDTO("Anderson", "anderson@gmail.com", "Anderson");
     }
     public static User toUser() {
-        return new User(UUID.randomUUID(), "name", "email", "password", UserType.ADMIN);
+        return new User(UUID.randomUUID(), "Anderson", "anderson@gmail.com", "Anderson", UserType.ADMIN);
     }
 }
