a70bac074f6205982dcf0451e650a13968e41741
Anderson Viana <andersonp.viana.contato@gmail.com>
2024-04-18 09:54:51 -0300

    feat: Leaving the class static and creating the to Optional User method

--- ms-user/src/main/java/com/anderson/msuser/mapper/UserMapper.java
+++ ms-user/src/main/java/com/anderson/msuser/mapper/UserMapper.java
@@ -6,18 +6,24 @@ import com.anderson.msuser.core.user.model.User;
 import com.anderson.msuser.api.validation.UserRequestDTO;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @Component
 public class UserMapper {
 
-    public UserDTO toUserDTO(UserRequestDTO dto) {
+    public static UserDTO toUserDTO(UserRequestDTO dto) {
         return new UserDTO(dto.name(), dto.email(), dto.password());
     }
 
-    public UserEntity toUserEntity(User user) {
+    public static UserEntity toUserEntity(User user) {
         return new UserEntity(user.getId(), user.getName(), user.getEmail(), user.getPassword(), user.getUserType());
     }
 
-    public User toUser(UserEntity userEntity) {
+    public static User toUser(UserEntity userEntity) {
         return new User(userEntity.getId(), userEntity.getName(), userEntity.getEmail(), userEntity.getPassword(), userEntity.getUserType());
+    }
+
+    public static Optional<User> toOptionalUser(Optional<UserEntity> userEntity) {
+        return userEntity.map(entity -> new User(entity.getId(), entity.getName(), entity.getEmail(), entity.getPassword(), entity.getUserType()));
     }
 }
