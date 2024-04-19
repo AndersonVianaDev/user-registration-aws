df16f47e0ea2462d3f940a1f0a62056762c0a8d5
Anderson Viana <andersonp.viana.contato@gmail.com>
2024-04-19 10:11:42 -0300

    feat: adding admin user

---
+++ ms-user/src/main/resources/db/migration/V2__insert_user_admin.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_users (id, name, email, password, user_type)
+VALUES
+    ('123e4567-e89b-12d3-a456-426614174000', 'admin', 'admin@gmail.com', '$2a$10$mOhWn/Svmq.O/wW8DgLoieUt2DYnak.pNPMCBlOEn3vKtNPBPPh4a', 1);
